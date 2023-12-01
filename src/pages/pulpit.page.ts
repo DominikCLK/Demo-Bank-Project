72822d8d137d3b54bb556266086d780ae3027dbd
DominikCLK <dominik.calak@gmail.com>
2023-12-02 00:03:48 +0100

    test: invalid data login test

--- src/pages/pulpit.page.ts
+++ src/pages/pulpit.page.ts
@@ -4,6 +4,7 @@ import { Page } from '@playwright/test';
 export class PulpitPage extends BasePage {
   url = '/pulpit.html';
   titleText = 'Demobank - Bankowość Internetowa - Pulpit';
+  userName = this.page.getByTestId('user-name');
 
   constructor(page: Page) {
     super(page);
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,8 @@
+import { randomLoginData } from '../src/factories/user.factory';
+import { UserName } from '../src/models/user.model';
 import { LoginPage } from '../src/pages/login.page';
 import { PulpitPage } from '../src/pages/pulpit.page';
-import { testUser } from '../src/test-data/user.data';
+import { loggedTestUser, testUser } from '../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
@@ -14,14 +16,36 @@ test.describe('Verify login', () => {
     await loginPage.goto();
   });
 
-  test('Verify that users can successfully log in with valid credentials. @DB-R01-01', async ({
+  test('Verify that users can successfully log in with valid credentials. @DB-R01-01 @DB-R01-02', async ({
     page,
   }) => {
+    // Arrange
+    const loggedUserData: UserName = {
+      userName: loggedTestUser.userName,
+    };
+    const userName = loggedUserData.userName;
+
     // Act
     await loginPage.login(testUser);
     const title = await pulpitPage.title();
 
     //Assert
+    expect(title).toContain(pulpitPage.titleText);
+    await expect(page).toHaveURL(pulpitPage.url);
+    await expect(pulpitPage.userName).toHaveText(userName);
+  });
+
+  test('Verify that users can not log in with ID less than 8 characters. @DB-R02-01', async ({
+    page,
+  }) => {
+    // Arrange
+    const invalidID = randomLoginData(7, 8);
+
+    // Act <--to do
+    await loginPage.login(invalidID);
+    const title = await pulpitPage.title();
+
+    //Assert <--to do
     expect(title).toContain(pulpitPage.titleText);
     await expect(page).toHaveURL(pulpitPage.url);
   });
