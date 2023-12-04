c875ca3109fc8468b142b0e83c5a9f17ed8ec87f
DominikCLK <dominik.calak@gmail.com>
2023-12-05 00:04:03 +0100

    test: new assert for successful transfer

--- tests/fastTransfer.spec.ts
+++ tests/fastTransfer.spec.ts
@@ -1,19 +1,26 @@
+import { generateRandomSentence } from '../src/factories/randomData.factory';
 import { LoginPage } from '../src/pages/login.page';
 import { PulpitPage } from '../src/pages/pulpit.page';
 import { testUser } from '../src/test-data/user.data';
-import { faker } from '@faker-js/faker/locale/pl';
+import { SubmitTransferView } from '../src/views/submitTransfer.view';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify Fast transfer flow', () => {
   let loginPage: LoginPage;
   let pulpitPage: PulpitPage;
+  let submitTransferView: SubmitTransferView;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
     pulpitPage = new PulpitPage(page);
+    submitTransferView = new SubmitTransferView(page);
 
     await loginPage.goto();
     await loginPage.login(testUser);
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.close();
   });
 
   test('Verify that users can successfully create a fast transfer @DB-R03-01 @DB-R03-02', async ({
@@ -21,15 +28,22 @@ test.describe('Verify Fast transfer flow', () => {
   }) => {
     // Arrange
     const title = await pulpitPage.title();
-
     const moneyValue = await pulpitPage.moneyValue.innerText();
-    const transferTitle = faker.lorem.sentence({ min: 3, max: 5 });
+    const transferTitle = generateRandomSentence(10);
 
     // Act
     await pulpitPage.fastTransfer(moneyValue, transferTitle);
+    await submitTransferView.submitTransferBtn.click();
 
     //Assert
     expect(title).toContain(pulpitPage.titleText);
     await expect(page).toHaveURL(pulpitPage.url);
+
+    await expect(pulpitPage.successfulTransferMessage).toContainText(
+      moneyValue,
+    );
+    await expect(pulpitPage.successfulTransferMessage).toContainText(
+      transferTitle,
+    );
   });
 });
