e11534a4d789df252d98296b5f22f0c2a1fe4c29
DominikCLK <dominik.calak@gmail.com>
2023-12-15 17:01:19 +0100

    chore: remove useless elements

--- src/views/submitTransfer.view.ts
+++ src/views/submitTransfer.view.ts
@@ -2,13 +2,5 @@ import { Page } from '@playwright/test';
 
 export class SubmitFastTransferView {
   submitTransferBtn = this.page.getByTestId('close-button');
-
-  submitViewTextFastTransfer = this.page.getByText('Przelew wykonany', {
-    exact: true,
-  });
-  submitViewTextPhoneTransfer = this.page.getByText('Doładowanie wykonane', {
-    exact: true,
-  });
-
   constructor(private page: Page) {}
 }
