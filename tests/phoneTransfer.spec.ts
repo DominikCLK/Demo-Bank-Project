ad9be0bba80a24ba03d935ae73ae2731a3ff7467
Dominik <75272795+DominikCLK@users.noreply.github.com>
2023-12-28 13:07:18 +0100

    Update phoneTransfer.spec.ts

--- tests/phoneTransfer.spec.ts
+++ tests/phoneTransfer.spec.ts
@@ -29,7 +29,7 @@ test.describe('Verify Phone transfer flow', () => {
     await page.close();
   });
 
-  test('Verify that users can successfully create a phone transfer @DB-R05-01 @DB-R05-02', async () => {
+  test('Verify that users can successfully create a phone transfer 1 @DB-R05-01 @DB-R05-02', async () => {
     // Arrange
     const amount = pulpitPage.randomPhoneAmountTopUp;
     const phone = pulpitPage.randomPhoneOption;
